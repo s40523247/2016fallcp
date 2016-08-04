a438d7571ee8f657cf633efced723d821797dd0f
mdecourse <mdecourse@gmail.com>
2016-08-05 00:21:28 +0800

    Updated from Brython Server: 8/5/2016 12:21:28 AM

--- week1.py
+++ week1.py
@@ -1,2 +1,8 @@
-for i in range(5):
+# 使用井字號開頭的這一行, 為註解, Python 解譯器不會執行
+'''
+以3個單引號, 或3個雙引號前後圈住的內容, 為多行註解, Python 解譯器也不會執行
+'''
+# 選擇一個變數, 名稱定為 repeat, 且將整數 10 與此變數對應
+repeat = 10
+for i in range(repeat):
     print("hello world!")
