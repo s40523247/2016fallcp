e2a8fcff8104f9125ba90b664d2fb10490ddfe26
scrum-1 <scrum1@mde.tw>
2016-08-05 00:58:48 +0800

    Updated from Brython Server: 8/5/2016 12:58:47 AM

--- week1.py
+++ week1.py
@@ -8,6 +8,7 @@
 # 使用井字號開頭的這一行, 為註解, Python 解譯器不會執行
 '''
 以3個單引號, 或3個雙引號前後圈住的內容, 為多行註解, Python 解譯器也不會執行
+https://blog.openshift.com/enabling-redis-for-your-app/
 '''
 # 選擇一個變數, 名稱定為 repeat, 且將整數 10 與此變數對應
 repeat = 10
