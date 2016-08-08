4a695e98614253a353026de66d1d9c2d0f75dcc3
mdecourse <mdecourse@gmail.com>
2016-08-08 22:42:18 +0800

    Updated from Brython Server: 8/8/2016 10:42:16 PM

--- week2.py
+++ week2.py
@@ -376,7 +376,7 @@ myapp.run()
 '''
 
 ########################################
-
+'''
 from ggame import App, Color, LineStyle, Sprite
 from ggame import CircleAsset
 
@@ -394,3 +394,36 @@ sprites = [Sprite(mycircle, (x, x*0.5 + 100)) for x in xcoordinates]
 
 myapp = App()
 myapp.run()
+'''
+#################################
+
+"""
+multiplication-table.py
+Author: Hayden Hatfield
+Credit: Mr. Denison
+Assignment:
+
+Write and submit a Python program that prints a multiplication table. The user 
+must be able to determine the width and height of the table before it is printed.
+
+The final multiplication table should look like this:
+
+Width of multiplication table: 10
+Height of multiplication table: 8
+
+  1   2   3   4   5   6   7   8   9  10
+  2   4   6   8  10  12  14  16  18  20
+  3   6   9  12  15  18  21  24  27  30
+  4   8  12  16  20  24  28  32  36  40
+  5  10  15  20  25  30  35  40  45  50
+  6  12  18  24  30  36  42  48  54  60
+  7  14  21  28  35  42  49  56  63  70
+  8  16  24  32  40  48  56  64  72  80
+"""
+Width = input("Width of multiplication table: ")
+Height = input("Height of multiplication table: ")
+for y in range(1, int(Height)+1 ):
+    for x in range(1, int(Width)+1 ):
+        print (y*x, end = " ")
+        pass
+    print()
