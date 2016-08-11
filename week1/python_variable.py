7fe99e9c32574d502b37ba209dad4a674f2c5777
mdecourse <mdecourse@gmail.com>
2016-08-11 20:14:43 +0800

    Create python_variable.py

---
+++ week1/python_variable.py
@@ -0,0 +1,16 @@
+# python variable 作為關鍵字
+'''
+Python variable
+
+Python keywords
+
+Python function 函式與重複迴圈
+
+Python for loop
+
+Python input function
+
+print function
+
+function result to print or return
+'''
