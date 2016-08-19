ce020afeda506b46183bef4e692d6b284eed0a4d
機械設計工程系課程 <mdecourse@gmail.com>
2016-08-19 22:12:36 +0800

    臺北市政府交通即時資料

--- smartcitymap.py
+++ smartcitymap.py
@@ -1,1 +1,4 @@
 # http://taipeicity.github.io/SmartCityMap/
+# https://github.com/taipeicity/SmartCityMap
+# https://taipeicity.github.io/traffic_realtime/
+# https://github.com/taipeicity/traffic_realtime
